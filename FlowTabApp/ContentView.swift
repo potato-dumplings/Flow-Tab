3c2521523c7720bb52a8c5a550e103fe812b6623
flow-tab-author <49275361+potato-dumplings@users.noreply.github.com>
2026-03-24 15:55:35 +0800

    Initial Commit

---
+++ FlowTabApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowTabApp
+//
+//  Created by lk on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowTabApp/FlowTabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowTabAppApp.swift
+//  FlowTabApp
+//
+//  Created by lk on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct FlowTabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
