18177af066e2fa68ccc062fd9b729bc469ed1fec
flow-tab-author <49275361+potato-dumplings@users.noreply.github.com>
2026-08-12 10:18:14 +0800

    refactor(sync): extract SYNC-036GY watchdog policy

--- FlowTabUITests/FlowTabUITests+SwitcherAndSearch.swift
+++ FlowTabUITests/FlowTabUITests+SwitcherAndSearch.swift
@@ -1,44 +1,6 @@
 import XCTest
 
-private enum FlowTabUITestSwitcherAndSearchWatchdogPolicy {
-    static let optionTabAppClickDismissal: TimeInterval = 2
-    static let controlTabWindowClickDismissal: TimeInterval = 2
-    static let searchResultClickDismissal: TimeInterval = 2
-    static let searchMockForegroundReadiness: TimeInterval = 10
-    static let spaceFixtureSearchResultPublication: TimeInterval = 5
-    static let searchHeaderProjection: TimeInterval = 10
-    static let searchMockSingleResultProjection: TimeInterval = 5
-    static let searchMockMultipleResultProjection: TimeInterval = 10
-    static let pointerInteractionForegroundReadiness: TimeInterval = 10
-    static let optionTabSingleAppRowProjection: TimeInterval = 5
-    static let optionTabAppRowCollectionProjection: TimeInterval = 10
-    static let optionTabSelectedAppProjection: TimeInterval = 3
-    static let compatibleBounds = [
-        optionTabAppClickDismissal,
-        controlTabWindowClickDismissal,
-        searchResultClickDismissal,
-        searchMockForegroundReadiness,
-        spaceFixtureSearchResultPublication,
-        searchHeaderProjection,
-        searchMockSingleResultProjection,
-        searchMockMultipleResultProjection,
-        pointerInteractionForegroundReadiness,
-        optionTabSingleAppRowProjection,
-        optionTabAppRowCollectionProjection,
-        optionTabSelectedAppProjection
-    ]
-}
-
 extension FlowTabUITests {
-    func testSwitcherAndSearchWatchdogPolicyPreservesCompatibleBounds() {
-        let policies = FlowTabUITestSwitcherAndSearchWatchdogPolicy.compatibleBounds
-        XCTAssertEqual(
-            policies,
-            [2, 2, 2, 10, 5, 10, 5, 10, 10, 5, 10, 3]
-        )
-        XCTAssertTrue(policies.allSatisfy { $0.isFinite && $0 > 0 })
-    }
-
     private var searchPointerHoverArguments: [String] {
         searchMockRuntimeArguments()
     }
